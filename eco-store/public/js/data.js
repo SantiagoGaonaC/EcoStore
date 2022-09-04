56328e15d043599338c5ebbac45223ad601b3fe6
SantiagoGaonaC <sgaonacarvajal@outlook.com>
2022-09-03 22:40:30 -0500

    first commit

---
+++ eco-store/public/js/data.js
@@ -0,0 +1,17 @@
+let data_tienda = [
+    {
+      id:"1",
+      nombre:"Aceite esencial de Clavo",
+      info_1:"12ML",
+      info_2:"Sale a: 665,83 €/L",
+      precio:"7,99 €",
+      img:"../public/img/src-eco-store/1.jpg",
+    },
+    {
+      id: "ioytrhndcv",
+      name: "Office Shirt",
+      price: 100,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-2.jpg",
+    },
+  ];
